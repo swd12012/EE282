569d112989ba743c7493d1e8023fc7c8ae6de759
Sam Du <swdu@uci.edu>
2020-12-09 12:57:56 -0800

    change fastqc files, remove redundant

--- project/scripts/fastqc_dir.sh
+++ project/scripts/fastqc_dir.sh
@@ -8,14 +8,14 @@ dir=$1
 shift
 
 
-scriptsdir="/data/homezvol2/swdu/ee282/project/scripts"
+scriptsdir="/data/class/ecoevo282/swdu/ee282/project/scripts"
 directory=$(readlink -f "$dir")
 
 for f in `$directory`*.gz
 do
 	echo "$f" 
  	out=$(dirname $f)
-        sbatch --wrap "$scriptsdir/fastqc.sh $f "
+        sbatch --wrap "$scriptsdir/fastqc.sub $f "
 
 done
 
